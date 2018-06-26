1367d9d2c7a39a8335596a6df4aab6827f52f326
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-06-26 14:12:06 +0300

    Add components for booking

---
+++ src/components/parts/BookingDashboard.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// Styles
+import styles from "./BookingDashboard.module.scss";
+
+
+class BookingDashboard extends React.Component {
+
+    render() {
+        console.log(styles);
+        return (
+            <div>
+                BookingDashboard
+            </div>
+        );
+    }
+}
+
+
+export default BookingDashboard;
