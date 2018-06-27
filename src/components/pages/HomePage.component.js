a1b4e03d4e0a8a56341717909acaa5b578d2bcfb
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2018-06-27 12:14:39 +0300

    Finish input phone logic

--- src/components/pages/HomePage.component.js
+++ src/components/pages/HomePage.component.js
@@ -7,8 +7,8 @@ class HomePage extends React.Component {
     render() {
         return (
             <div>
-                <Link to="/input">Input</Link>
-                <Link to="/booking">Booking</Link>
+                <Link to="/input" style={{ fontSize: 17, color: "white", display: 'block', padding: 10 }}>Javascript Exercise 1 Phone input field</Link>
+                <Link to="/booking" style={{ fontSize: 17, color: "white", display: 'block', padding: 10 }}>Javascript Exercise 2 Booking app</Link>
             </div>
         );
     }
--- src/components/pages/InputPage.component.js
+++ src/components/pages/InputPage.component.js
@@ -1,14 +1,77 @@
 import React from 'react';
+// MobX
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 
 
 class InputPage extends React.Component {
 
+    inputValue = observable.box("");
+
+    prefix = "+1";
+
+    symbolsLimit = 10;
+
+
+    get clearInputValue() { return this.removePrefix(this.inputValue.get()); };
+
+
+    removeNonNumbers(str="") { return str.replace(/[^0-9]/g, ""); };
+
+    removePrefix(str="") { return str.replace(/^\+1{1}/g, ""); };
+
+    getSpaces(count=0) { return Array.from(Array(count)).map(()=>" ").join(''); };
+
+
+    onInputChange = e => {
+        const newValue = this.removeNonNumbers(e.target.value);
+
+        if(!newValue) return this.inputValue.set("");
+        if(newValue === this.clearInputValue) return this.inputValue.set(this.prefix + newValue.substring(0, newValue.length-1));
+        this.inputValue.set(this.prefix + newValue);
+    };
+
+
+    validate = str => {
+        str = this.removePrefix(str);
+
+        if(str.length > this.symbolsLimit) return str;
+
+        let countryCode = str.substring(0, 3);
+        const countryCodeSpacesLength = countryCode ? 3 - countryCode.length : 0;
+        countryCode = countryCode ? `(${countryCode}${this.getSpaces(countryCodeSpacesLength)})` : "";
+
+        const firstPart = str.substring(3, 6) ? ` ${str.substring(3, 6)}` : "";
+        const secondPart = str.substring(6, 10) ? `-${str.substring(6, 10)}` : "";
+
+        return `${countryCode}${firstPart}${secondPart}`;
+    };
+
+
+    onKeyUp = ()=> {
+        if(this.clearInputValue.length > 3 ) return;
+
+        const countryCodeSpacesLength = this.clearInputValue.length + 1;
+        this.inputRef.setSelectionRange(countryCodeSpacesLength, countryCodeSpacesLength);
+    };
+
+
     render() {
         return (
-            <div>Input</div>
+            <div>
+                <input type="text"
+                       placeholder="Enter phone number"
+                       ref={ (node)=> this.inputRef = node }
+                       onChange={ this.onInputChange }
+                       onKeyUp={ this.onKeyUp }
+                       value={ this.validate(this.inputValue.get()) } />
+                <br/>
+                <br/>
+                Value: { this.inputValue.get() }
+            </div>
         );
     }
 }
 
 
-export default InputPage;
+export default observer(InputPage);
